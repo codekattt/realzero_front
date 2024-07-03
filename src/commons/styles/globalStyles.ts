c737c09e6d0598eec61ee797ffd4a7c238f027fa
codekattt <widkid00@naver.com>
2024-07-03 17:38:43 +0900

    add_fontFamily

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -4,13 +4,15 @@ export const globalStyles = css`
   * {
     margin: 0;
     box-sizing: border-box;
-    font-family: 'Pretendard-Regular';
+    font-family: 'Pretendard-Regular', 'Helvetica', 'Arial', sans-serif;
   }
 
   @font-face {
     font-family: 'Pretendard-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
-      format('woff');
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff2')
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+        format('woff');
     font-weight: 400;
     font-style: normal;
   }
